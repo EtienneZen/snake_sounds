5a2bfc544e9d0c658dea38eb8fe1f37db303e230
Stephen <stephensaundersj@gmail.com>
2020-10-22 18:33:57 -0500

    idk

---
+++ test.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+list=[]
+for i in range (334):
+    list.append(3 * i)
+for g in range (200):
+    if (5 * g) in list:
+        continue
+    else:
+        list.append (5 * g)
+print (sum (list))
+=======
+import sys; x='foo'; sys.stdout.write(x + '\n');
+>>>>>>> cd7319505c189ed2439edf04e8608d8093aa4a09
