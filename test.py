38ff4813af2864e1a674527b5862460cd89160b6
Stephen <stephensaundersj@gmail.com>
2020-10-22 18:52:54 -0500

    i still dont know

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
-list=[]
-for i in range (334):
-    list.append(3 * i)
-for g in range (200):
-    if (5 * g) in list:
-        continue
-    else:
-        list.append (5 * g)
-print (sum (list))
-=======
-import sys; x='foo'; sys.stdout.write(x + '\n');
->>>>>>> cd7319505c189ed2439edf04e8608d8093aa4a09
+tup1 = (1, 2, 3);
+print (tup1)
