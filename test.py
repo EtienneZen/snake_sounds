c17368ca461d761b7395003b62bf3271b822e191
Stephen <stephensaundersj@gmail.com>
2020-10-23 09:25:45 -0500

    more practice

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import math
+def fahrenheit_to_celsius (temp):       #Converts fahrenheit to celsius
+    """Converts fahrenheit to celsius"""
+    return int((temp - 32) * (5 / 9))
 
-def circle_perimeter(radius): #calculates the perimeter of a cirlce given the radius
-    """Calculates the perimeter of a cirlce given the radius."""
-    return radius * math.pi * 2
+def celsius_to_fahrenheit (temp):       #Converts celsius to fahrenheit
+    """Converts celsius to fahrenheit"""
+    return int(temp * (9/5) + 32)
+
+
+if __name__ == "__main__":
+    print(celsius_to_fahrenheit(100))
